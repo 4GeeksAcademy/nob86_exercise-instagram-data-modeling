02d7cef8e749edf8a0ab7a5a1e612c1264ead34d
nob86 <gustavoadallacroce@gmail.com>
2024-10-20 15:57:03 +0000

    creeria que esta bien

--- src/models.py
+++ src/models.py
@@ -1,11 +1,66 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String, (50), unique = True, nullable = False)
+    firstname = Column(String, (50))
+    lastname = Column(String, (50))
+    email = Column(String, (50), unique = True, nullable = False)
+
+    posts = relationship('Post', back_populates = 'user')
+    comments = relationship('Comment', back_populates = 'author')
+    followers = relationship('Follower', foreign_keys = 'follower.user_from_id', backref = 'followers')
+    followings = relationship('Follower', foreign_keys = 'follower.user_to_id', backref = 'followings')
+    
+    
+class Post(Base):
+    __tablename__ = 'post'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+    user = relationship('User', back_populates = 'posts')
+    media = relationship('Media', back_populates = 'post')
+    comments = relationship('Comment', back_populates = 'post')
+    
+
+class Comment(Base):
+    __tablename__ = 'comment'
+
+    id = Column(Integer, primary_key=True)
+    content = Column(String(250))
+    autor_id = Column(Integer, ForeignKey('user.id'))
+    post_id =  Column(Integer,ForeignKey('post.id'))
+    
+    author = relationship('User', back_populates = 'comments')
+    post = relationship('Post',  back_populates = 'comments')
+
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    type = Column(Enum('video', 'imagen'), nullable = False)
+    url = Column(String(250))
+    post_id = Column(Integer,ForeignKey('post.id'))
+
+    post = relationship('Post', back_populates = 'media')
+
+
+
+class Follower(Base):
+    __tablename__ = 'follower'
+    user_from_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    user_to_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+
+
 
 class Person(Base):
     __tablename__ = 'person'
